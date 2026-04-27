7ab0504eece499bcd05e1c47ad0f42ea23341b6f
gangmin2 <min0115b@gmail.com>
2026-04-28 06:43:34 +0900

    feat(db): RDS SSL 연결 자동 처리

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,7 +1,12 @@
 import pg from 'pg'
 import { config } from '../config.js'
 
-export const pool = new pg.Pool({ connectionString: config.databaseUrl })
+const usesRds = /\.rds\.amazonaws\.com/.test(config.databaseUrl)
+
+export const pool = new pg.Pool({
+  connectionString: config.databaseUrl,
+  ssl: usesRds ? { rejectUnauthorized: false } : false,
+})
 
 export function query(text, params) {
   return pool.query(text, params)
